27f4867ba2988802cce7182a3e683c2ea162eab3
666WXY666 <1293739194@qq.com>
2021-01-13 19:01:31 +0800

    Github CI Auto Builder at 2021-01-13 19:01:30 +08:00

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
+// 彩色标签云
+function colortag(){
+$("ul.tag-list li").each(function(){
+    let random = Math.floor(Math.random()*5+1);
+    if(random == 2){
+        $(this).find('a').addClass('tagred');
+    } else if ( random == 3 ){
+        $(this).find('a').addClass('tagyellow');
+    } else if ( random == 4 ){
+        $(this).find('a').addClass('tagblue');
+    } else if ( random == 5 ){
+        $(this).find('a').addClass('taggreen');
+    } else if ( random == 2 ){
+        $(this).find('a').addClass('tagpurple');
+    } else if ( random == 1 ){
+        $(this).find('a').addClass('taggrown');
+    }
+ 
+});}
+ 
+colortag();
+
 // 函数防抖 (只执行最后一次点击)
 var Debounce = (fn, t) => {
   const delay = t || 25;
@@ -353,3 +375,52 @@ var Debounce = (fn, t) => {
     100);
   }
 })(jQuery);
+
+// 消息Message.js
+$.extend({
+    message: function(e) {
+        var n = {
+            title: "",
+            message: " 操作成功",
+            time: "3000",
+            type: "success",
+            showClose: !0,
+            autoClose: !0,
+            onClose: function() {}
+        };
+        "string" == typeof e && (n.message = e),
+        "object" == typeof e && (n = $.extend({},
+        n, e));
+        var i, o, s, t = n.showClose ? '<div class="c-message--close">×</div>': "",
+        a = "" !== n.title ? '<h2 class="c-message__title">' + n.title + "</h2>": "",
+        c = '<div class="c-message animated animated-lento slideInRight"><i class=" c-message--icon c-message--' + n.type + '"></i><div class="el-notification__group">' + a + '<div class="el-notification__content">' + n.message + "</div>" + t + "</div></div>",
+        m = $("body"),
+        d = $(c);
+        o = function() {
+            d.addClass("slideOutRight"),
+            d.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+            function() {
+                s()
+            })
+        },
+        s = function() {
+            d.remove(),
+            n.onClose(n),
+            clearTimeout(i)
+        },
+        $(".c-message").remove(),
+        m.append(d),
+        d.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+        function() {
+            d.removeClass("messageFadeInDown")
+        }),
+        m.one("click", ".c-message--close",
+        function(e) {
+            o()
+        }),
+        n.autoClose && (i = setTimeout(function() {
+            o()
+        },
+        n.time))
+    }
+});
